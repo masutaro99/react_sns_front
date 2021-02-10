94eab520ee828f0032da4b671f04542cbf38e26f
masutaro99 <maskenpa1001@gmail.com>
2021-02-11 00:05:51 +0900

    update: update user registration form

--- sns_react/src/components/auth/Registration.js
+++ sns_react/src/components/auth/Registration.js
@@ -5,25 +5,32 @@ export default function Registration() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [passwordConfirmation, setPasswordConfirmation] = useState("")
+  const handleSubmit = (event) => {
+    console.log("イベント発火")
+    event.preventDefault()
+  }
   return (
     <div>
       <p>新規登録</p>
       {/* 追加 */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="email"
                name="email"
                placeholder="メールアドレス"
                value={email}
+               onChange={event => setEmail(event.target.value)}
         />
         <input type="password"
                name="password"
                placeholder="パスワード"
                value={password}
+               onChange={event => setPassword(event.target.value)}
         />
         <input type="password"
                name="password_confirmation"
                placeholder="確認用パスワード"
                value={passwordConfirmation}
+               onChange={event => setPasswordConfirmation(event.target.value)}
         />
         <button type="submit">登録</button>
       </form>
