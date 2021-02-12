89b7fd2004be966142ea6928ca1227e041a0aa78
masutaro99 <maskenpa1001@gmail.com>
2021-02-13 00:36:27 +0900

    add: create login and create user form using material ui

--- sns_react/src/components/Login.js
+++ sns_react/src/components/Login.js
@@ -16,7 +16,6 @@ import {
   INPUT_EDIT,
   TOGGLE_MODE,
 } from "./actionTypes";
-import { Input } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -104,14 +103,175 @@ const loginReducer = (state, action) => {
   }
 };
 
-const Login = () => {
+const Login = (props) => {
   const classes = useStyles();
   const [state, dispatch] = useReducer(loginReducer, initialState);
+
+  const inputChangedLog = () => (event) => {
+    const cred = state.credentialsLog;
+    cred[event.target.name] = event.target.value;
+    dispatch({
+      type: INPUT_EDIT,
+      inputName: "state.creditialLog",
+      payload: cred,
+    });
+  };
+
+  const inputChangedReg = () => (event) => {
+    const cred = state.credentialsReg;
+    cred[event.target.name] = event.target.value;
+    dispatch({
+      type: INPUT_EDIT,
+      inputName: "state.creditialReg",
+      payload: cred,
+    });
+  };
+
+  const login = async (event) => {
+    event.preventDefault();
+    if (state.isLogView) {
+      try {
+        dispatch({ type: START_FETCH });
+        console.log(state.credentialsLog);
+        // const res = await axios.post(
+        //   "http://127.0.0.0.0:3000/login",
+        //   state.credentialsLog,
+        //   {
+        //     headers: { "Content-Type": "application/json" },
+        //   }
+        // );
+        // props.cookies.set("current-token", res.data.token);
+        // res.data.token
+        //   ? (window.location.href = "/profiles")
+        //   : (window.location.href = "/");
+        dispatch({ type: FETCH_SUCCESS });
+      } catch {
+        dispatch({ type: ERROR_CATCHED });
+      }
+    } else {
+      try {
+        dispatch({ type: START_FETCH });
+        // await axios.post(
+        //   "http://localhost:3000/v1/users/",
+        //   state.credentialsReg,
+        //   {
+        //     headers: { "Content-Type": "application/json" },
+        //   }
+        // );
+        console.log(state.credentialsReg);
+        dispatch({ type: FETCH_SUCCESS });
+        dispatch({ type: TOGGLE_MODE });
+      } catch {
+        dispatch({ type: ERROR_CATCHED });
+      }
+    }
+  };
+
+  const toggleView = () => {
+    dispatch({ type: TOGGLE_MODE });
+  };
+
   return (
-    <div>
-      <h3>login</h3>
-    </div>
+    <Container maxWidth="xs">
+      <form onSubmit={login}>
+        <div className={classes.paper}>
+          {state.isLoading && <CircularProgress />}
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography variant="h5">
+            {state.isLoginView ? "Login" : "Register"}
+          </Typography>
+          {state.isLoginView ? (
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              label="Email"
+              value={state.credentialsLog.email}
+              onChange={inputChangedLog()}
+              autoFocus
+            />
+          ) : (
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              label="Email"
+              value={state.credentialsLog.email}
+              onChange={inputChangedReg()}
+              autoFocus
+            />
+          )}
+          {state.isLoginView ? (
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              label="password"
+              type="password"
+              value={state.credentialsLog.password}
+              onChange={inputChangedLog()}
+            />
+          ) : (
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              label="password"
+              type="password"
+              value={state.credentialsLog.password}
+              onChange={inputChangedReg()}
+            />
+          )}
+          <span className={classes.spanError}>{state.error}</span>
+          {state.isLoginView ? (
+            !state.credentialsLog.password || !state.credentialsLog.username ? (
+              <Button
+                className={classes.submit}
+                type="submit"
+                fullWidth
+                disabled
+                variant="contained"
+                color="primary"
+              ></Button>
+            ) : (
+              <Button
+                className={classes.submit}
+                type="submit"
+                fullWidth
+                disabled
+                variant="contained"
+                color="primary"
+              ></Button>
+            )
+          ) : !state.credentialsLog.password ||
+            !state.credentialsLog.username ? (
+            <Button
+              className={classes.submit}
+              type="submit"
+              fullWidth
+              disabled
+              variant="contained"
+              color="primary"
+            ></Button>
+          ) : (
+            <Button
+              className={classes.submit}
+              type="submit"
+              fullWidth
+              disabled
+              variant="contained"
+              color="primary"
+            ></Button>
+          )}
+          <span onClick={() => toggleView()} className={classes.span}>
+            {state.isLoginView ? "Create Account" : "Back to lobin ?"}
+          </span>
+        </div>
+      </form>
+    </Container>
   );
 };
 
-export default Login;
+export default withCookies(Login);
