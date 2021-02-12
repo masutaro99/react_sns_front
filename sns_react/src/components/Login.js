87686d10c9289001e99f22832791741d67cae551
masutaro99 <maskenpa1001@gmail.com>
2021-02-12 15:42:17 +0900

    add: create Login form design

--- sns_react/src/components/Login.js
+++ sns_react/src/components/Login.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, {useReducer} from 'react';
+import { withCookies } from 'react-cookie';
+import axios from 'axios';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  span: {
+    color: 'teal',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  spanError: {
+    color: 'fuchsia',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    margintop: 10,
+  },
+}));
+
 
 const Login = () => {
+  const classes = useStyles();
   return (
     <div>
-      
+      <h3>login</h3>
     </div>
   )
 }
